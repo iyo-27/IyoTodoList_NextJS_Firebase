f43bda4ae8d0e1d6de750b2ed47012b894abb732
iyo.nina27@gmail.com <iyo.nina27@gmail.com>
2023-12-07 21:19:53 +0900

    タスク期限によって表示色を変えるように修正した

--- app/memo/TaskDetail.js
+++ app/memo/TaskDetail.js
@@ -7,17 +7,25 @@ function TaskDetail(props){
     const Checkboxmargin = {
         margin: '16px'
     }
-    
+
     // 期限の変換
     let d = new Date(props.deadline * 1000)
     console.log('TaskDetailまで来てる id：' + props.id + ', message：' + props.message + ', deadline：' + d.getDay())
 
+    // 期限を過ぎている場合、赤色表示
+    let deadLineStyle = 'nes-text '
+    if (d.getTime() < (new Date()).getTime()) {
+        deadLineStyle += 'is-primary'
+    } else {
+        deadLineStyle += 'is-error'
+    }
+
     return(
-        <label className="listItem">
-            <input type="checkbox" className="nes-checkbox" style={Checkboxmargin} id={props.id}/>
+        <label className='listItem'>
+            <input type='checkbox' className='nes-checkbox' style={Checkboxmargin} id={props.id}/>
             <span>{props.message}</span>
             <span>&nbsp;</span>
-            <span><label className="nes-text is-primary">期限</label>：{(d.getMonth() + 1) + '/' + d.getDate()}</span>
+            <span><label className={deadLineStyle}>期限</label>：{(d.getMonth() + 1) + '/' + d.getDate()}</span>
         </label>
     )
 }
